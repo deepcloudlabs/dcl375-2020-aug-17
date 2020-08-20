dedd528a8e6f0b6f24052e76abb0fe0b1c3b7056
deepcloudlabs <deepcloudlabs@gmail.com>
2020-08-20 12:13:25 +0300

    implement @PostMapping, @PutMapping, @PatchMapping, @DeleteMapping in CustomerConstroller

--- crm-rest-api/src/main/java/com/example/crm/controller/CustomerController.java
+++ crm-rest-api/src/main/java/com/example/crm/controller/CustomerController.java
@@ -1,12 +1,16 @@
 package com.example.crm.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,24 +26,39 @@ import com.example.crm.service.CustomerService;
 @CrossOrigin
 // http://localhost:4001/crm/api/v1/customers (1) ✔
 public class CustomerController {
-	@Autowired private CustomerService customerService;
-	
-    // GET http://localhost:4001/crm/api/v1/customers/11111111110
+	@Autowired
+	private CustomerService customerService;
+
+	// GET http://localhost:4001/crm/api/v1/customers/11111111110
 	@GetMapping("{identity}")
 	public Customer getCustomerByIdentity(@PathVariable String identity) {
 		return customerService.findCustomerByIdentity(identity);
 	}
-	
+
 	// GET http://localhost:4001/crm/api/v1/customers?page=10&size=25
-	@GetMapping(params = {"page", "size"})
-	public List<Customer> getCustomers(
-			@RequestParam(name = "page", required = true, defaultValue = "0") int page, 
-			@RequestParam(name = "size", required = true, defaultValue = "10") int size){
+	@GetMapping(params = { "page", "size" })
+	public List<Customer> getCustomers(@RequestParam(name = "page", required = true, defaultValue = "0") int page,
+			@RequestParam(name = "size", required = true, defaultValue = "10") int size) {
 		return customerService.findCustomersWithPagination(page, size);
 	}
-	
+
 	@PostMapping
 	public void addCustomer(@RequestBody Customer customer) {
 		customerService.createCustomer(customer);
+	}
+
+	@PutMapping
+	public void updateCustomer(@RequestBody Customer customer) {
+		customerService.updateCustomer(customer);
+	}
+
+	@PatchMapping("{identity}")
+	public void patchCustomer(@PathVariable String identity, @RequestBody Map<String, Object> patchValues) {
+		customerService.patchCustomer(identity, patchValues);
+	}
+	
+	@DeleteMapping("{identity}")
+	public Customer deleteCustomerByIdentity(@PathVariable String identity) {
+		return customerService.removeCustomerByIdentity(identity);
 	}
 }
--- crm-rest-api/src/main/java/com/example/crm/service/CustomerService.java
+++ crm-rest-api/src/main/java/com/example/crm/service/CustomerService.java
@@ -1,6 +1,10 @@
 package com.example.crm.service;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +36,52 @@ public class CustomerService {
 		if( customerRepository.findById(identity).isPresent())
 			throw new IllegalArgumentException("Customer with identity "+identity+" already exists." );
 		customerRepository.save(customer);
+	}
+
+	@Transactional
+	public void updateCustomer(Customer customer) {
+		var identity = customer.getIdentity();
+		var optionalCustomer = customerRepository.findById(identity);
+		if( optionalCustomer.isEmpty())
+			throw new IllegalArgumentException("Customer with identity "+identity+" does not exist." );
+		var managedCustomer = optionalCustomer.get();
+		managedCustomer.setFullname(customer.getFullname());
+		managedCustomer.setPhoto(customer.getPhoto());
+		managedCustomer.setAddress(customer.getAddress());
+		// customerRepository.save(managedCustomer);
+	}
+
+	@Transactional
+	public void patchCustomer(String identity, Map<String, Object> patchValues) {
+		var optionalCustomer = customerRepository.findById(identity);
+		if( optionalCustomer.isEmpty())
+			throw new IllegalArgumentException("Customer with identity "+identity+" does not exist." );
+		var managedCustomer = optionalCustomer.get();
+		var clazz = Customer.class;
+		for (var entry : patchValues.entrySet()) {
+			// Use reflection to update managedCustomer
+			Field declaredField;
+			try {
+				declaredField = clazz.getDeclaredField(entry.getKey());
+				if (Objects.nonNull(declaredField)) {
+					declaredField.setAccessible(true);
+					declaredField.set(managedCustomer, entry.getValue());
+					declaredField.setAccessible(false);
+				}
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				System.err.println("Exception: "+e.getMessage());
+			}
+		}
+		// customerRepository.save(managedCustomer);
+	}
+
+	@Transactional
+	public Customer removeCustomerByIdentity(String identity) {
+		Supplier<IllegalArgumentException> illegalArgumentException = () -> new IllegalArgumentException("Cannot find customer with identity "+identity);
+		var customer = customerRepository.findById(identity)
+				                 .orElseThrow(illegalArgumentException);
+		customerRepository.delete(customer);
+		return customer;
 	}
 
 }
