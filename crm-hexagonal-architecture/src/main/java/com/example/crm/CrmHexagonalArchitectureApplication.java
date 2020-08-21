8298939ec508d29611ffa58603f979254d08f2f1
deepcloudlabs <deepcloudlabs@gmail.com>
2020-08-21 06:25:00 +0300

    add crm boot application

---
+++ crm-hexagonal-architecture/src/main/java/com/example/crm/CrmHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmHexagonalArchitectureApplication.class, args);
+	}
+
+}
